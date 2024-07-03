262282d328f48ffaf330e337d0ae42bf7bea2488
Sanchit Kedia <sanchitkedia1998@gmail.com>
2024-07-03 13:00:02 -0400

    Setup Moveit

---
+++ fanuc_moveit_config/launch/fanuc_move_group.launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch.actions import OpaqueFunction
+
+import os
+
+from launch_ros.actions import Node
+
+from moveit_configs_utils import MoveItConfigsBuilder
+
+from ament_index_python.packages import get_package_share_directory
+
+def launch_setup(context, *args, **kwargs):
+    
+    urdf = os.path.join(get_package_share_directory("fanuc_description"), "urdf/fanuc.urdf.xacro")
+
+    moveit_config = (
+        MoveItConfigsBuilder("fanuc", package_name="fanuc_moveit_config")
+        .robot_description(urdf)
+        .robot_description_semantic(file_path="config/fanuc.srdf")
+        .trajectory_execution(file_path="config/controllers.yaml")
+        .planning_pipelines(pipelines=["ompl"])
+        .to_moveit_configs()
+    )
+    
+    # Move group node
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        # remappings=[
+        #     ('/joint_states', '/fanuc/joint_states')             
+        # ],
+        parameters=[
+            moveit_config.to_dict(),
+            {"use_sim_time": True}
+        ],
+    )   
+
+    nodes_to_start = [
+        move_group_node
+    ]
+
+    return nodes_to_start
+
+
+def generate_launch_description():
+    declared_arguments = []
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
